12defccfb97b775fa3e051b5997586e22e3bada5
Pvtwuyver <pvtwuyver@gmail.com>
2017-06-08 13:35:25 +0200

    campsites added

--- calendar.js
+++ calendar.js
@@ -80,7 +80,7 @@ function drawPark() {
         .attr("d", pathMonth);
 
 
-    d3.csv("busyness_by_type.csv", function(error, csv) {
+    d3.csv("/data/busyness_by_type.csv", function(error, csv) {
         if (error) throw error;
 
         var data = d3.nest()
@@ -91,7 +91,7 @@ function drawPark() {
                 return (d[0].Total)
             }) // Total busyness in park
             .object(csv);
-        console.log(csv)
+        
         rect.filter(function(d) {
                 return d in data;
             })
@@ -184,7 +184,7 @@ function drawType1() {
         .attr("d", pathMonth);
 
 
-    d3.csv("busyness_by_type.csv", function(error, csv) {
+    d3.csv("/data/busyness_by_type.csv", function(error, csv) {
         if (error) throw error;
 
         var data = d3.nest()
@@ -195,7 +195,7 @@ function drawType1() {
                 return (d[0].One)
             }) // type 1 busyness in park
             .object(csv);
-        console.log(csv)
+        
         rect.filter(function(d) {
                 return d in data;
             })
@@ -288,7 +288,7 @@ function drawType2() {
         .attr("d", pathMonth);
 
 
-    d3.csv("busyness_by_type.csv", function(error, csv) {
+    d3.csv("/data/busyness_by_type.csv", function(error, csv) {
         if (error) throw error;
 
         var data = d3.nest()
@@ -299,7 +299,7 @@ function drawType2() {
                 return (d[0].Two)
             }) // type 2 busyness in park
             .object(csv);
-        console.log(csv)
+  
         rect.filter(function(d) {
                 return d in data;
             })
@@ -392,7 +392,7 @@ function drawType3() {
         .attr("d", pathMonth);
 
 
-    d3.csv("busyness_by_type.csv", function(error, csv) {
+    d3.csv("/data/busyness_by_type.csv", function(error, csv) {
         if (error) throw error;
 
         var data = d3.nest()
@@ -403,7 +403,7 @@ function drawType3() {
                 return (d[0].Three)
             }) // type 3 busyness in park
             .object(csv);
-        console.log(csv)
+        
         rect.filter(function(d) {
                 return d in data;
             })
@@ -496,7 +496,7 @@ function drawType4() {
         .attr("d", pathMonth);
 
 
-    d3.csv("busyness_by_type.csv", function(error, csv) {
+    d3.csv("/data/busyness_by_type.csv", function(error, csv) {
         if (error) throw error;
 
         var data = d3.nest()
@@ -507,7 +507,7 @@ function drawType4() {
                 return (d[0].Four)
             }) // type 4 busyness in park
             .object(csv);
-        console.log(csv)
+        
         rect.filter(function(d) {
                 return d in data;
             })
@@ -600,7 +600,7 @@ function drawType5() {
         .attr("d", pathMonth);
 
 
-    d3.csv("busyness_by_type.csv", function(error, csv) {
+    d3.csv("/data/busyness_by_type.csv", function(error, csv) {
         if (error) throw error;
 
         var data = d3.nest()
@@ -611,7 +611,7 @@ function drawType5() {
                 return (d[0].Five)
             }) // type 5 busyness in park
             .object(csv);
-        console.log(csv)
+        
         rect.filter(function(d) {
                 return d in data;
             })
@@ -704,7 +704,7 @@ function drawType6() {
         .attr("d", pathMonth);
 
 
-    d3.csv("busyness_by_type.csv", function(error, csv) {
+    d3.csv("/data/busyness_by_type.csv", function(error, csv) {
         if (error) throw error;
 
         var data = d3.nest()
@@ -715,7 +715,7 @@ function drawType6() {
                 return (d[0].Six)
             }) // type 6 busyness in park
             .object(csv);
-        console.log(csv)
+        
         rect.filter(function(d) {
                 return d in data;
             })
@@ -808,7 +808,7 @@ function drawTypeP() {
         .attr("d", pathMonth);
 
 
-    d3.csv("busyness_by_type.csv", function(error, csv) {
+    d3.csv("/data/busyness_by_type.csv", function(error, csv) {
         if (error) throw error;
 
         var data = d3.nest()
@@ -819,7 +819,7 @@ function drawTypeP() {
                 return (d[0].Ranger)
             }) // type P busyness in park
             .object(csv);
-        console.log(csv)
+   
         rect.filter(function(d) {
                 return d in data;
             })
@@ -847,7 +847,126 @@ function drawTypeP() {
             "H" + (w0 + 1) * cellSize + "Z";
     }
 };
+
+function drawAllCamps() {
+    var width = 960,
+        height = 136,
+        cellSize = 17;
+
+    var color = d3.scaleLinear()
+        .domain([1, 181]) // MUST BE RELATIVE TO MIN AND MAX
+        .range(["#fee0d2", "#de2d26"]);
+
+      // var svg = d3.select("#calendar");
+      //   if (svg !== undefined)
+      //       svg.remove();
+
+    var svg = d3.select("#allCamps")
+        .selectAll("svg")
+        .data(d3.range(2015, 2017))
+        .enter().append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + ((width - cellSize * 53) / 2) + "," + (height - cellSize * 7 - 1) + ")");
+
+    svg.append("text")
+        .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .attr("text-anchor", "middle")
+        .text(function(d) {
+            return d;
+        });
+
+    svg.append("text")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 15)
+        .attr('x', 10)
+        .attr('y', -5)
+        .attr("text-anchor", "under")
+        .text("Vehicles in all Campsites");
+
+
+    var rect = svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#ccc")
+        .selectAll("rect")
+        .data(function(d) {
+            return d3.timeDays(new Date(d, 0, 1), new Date(d + 1, 0, 1));
+        })
+        .enter().append("rect")
+        .attr("width", cellSize)
+        .attr("height", cellSize)
+        .attr("x", function(d) {
+            return d3.timeWeek.count(d3.timeYear(d), d) * cellSize;
+        })
+        .attr("y", function(d) {
+            return d.getDay() * cellSize;
+        })
+        .datum(d3.timeFormat("%d/%m/%Y"));
+
+    svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#000")
+        .selectAll("path")
+        .data(function(d) {
+            return d3.timeMonths(new Date(d, 0, 1), new Date(d + 1, 0, 1));
+        })
+        .enter().append("path")
+        .attr("d", pathMonth);
+
+
+    d3.csv("Map3.csv", function(error, csv) {
+        if (error) throw error;
+
+        var data = d3.nest()
+            .key(function(d) {
+                return d.Day;
+            })
+            .rollup(function(d) {
+                return (d[0].Total)
+            }) // type P busyness in park
+            .object(csv);
+   
+        rect.filter(function(d) {
+                return d in data;
+            })
+            .attr("fill", function(d) {
+                return color(data[d]);
+            })
+            .append("title")
+            .text(function(d) {
+                return d + ": " + data[d];
+            });
+    });
+
+
+
+    function pathMonth(t0) {
+        var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
+            d0 = t0.getDay(),
+            w0 = d3.timeWeek.count(d3.timeYear(t0), t0),
+            d1 = t1.getDay(),
+            w1 = d3.timeWeek.count(d3.timeYear(t1), t1);
+        return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize +
+            "H" + w0 * cellSize + "V" + 7 * cellSize +
+            "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize +
+            "H" + (w1 + 1) * cellSize + "V" + 0 +
+            "H" + (w0 + 1) * cellSize + "Z";
+    }
+};
+
 function getOption() {
     var obj = document.getElementById("mySelect");
-    console.log(obj.value);
+    if (obj.value == "Campsites") {
+      console.log("drawCampsites")
+      drawAllCamps();
+    }
+    if (obj.value == "Entrances") {
+      console.log("drawEntrances")
+    }
+    if (obj.value == "Total") {
+      console.log("drawTotals")
+    }
 }
