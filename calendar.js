f2ad291848bd66085a51b4470867e428ad3fe29c
Pvtwuyver <pvtwuyver@gmail.com>
2017-06-09 15:10:59 +0200

    camping 0 en 1 added

--- calendar.js
+++ calendar.js
@@ -7,6 +7,21 @@
 
 // start with overview traffic on window load
 window.onload = function() {
+
+// d3.queue()
+//         .defer(d3.csv, "data/busyness_by_type.csv", function(error, csv) {
+//             if (error) throw error;
+//             // draw calendars 
+//             drawMap(worldpopulation);
+//         })
+//         .defer(d3.json, "landcodes.json", function(error, agegroups) {
+//             if (error) throw error;
+
+//             globalAgegroups = agegroups;
+
+//         })
+//         .await(ready);
+
  showTotal();
 };
 // get User selection and select appropriate function to draw svg's
@@ -41,6 +56,8 @@ function showTotal(){
 // show all traffic in the campings
 function showCampings(){
     drawAllCamps();
+    drawCamp0();
+    drawCamp1();
     // add Camping 1, 2 etc
 }
 
@@ -987,4 +1004,211 @@ function drawAllCamps() {
             "H" + (w0 + 1) * cellSize + "Z";
     }
 };
+function drawCamp0() {
+    var width = 960,
+        height = 136,
+        cellSize = 10;
 
+    var color = d3.scaleLinear()
+        .domain([0, 22]) // MUST BE RELATIVE TO MIN AND MAX
+        .range(["#fee0d2", "#de2d26"]);
+
+    var svg = d3.select("#camp0")
+        .selectAll("svg")
+        .data(d3.range(2015, 2017))
+        .enter().append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + ((width - cellSize * 53) / 2) + "," + (height - cellSize * 7 - 1) + ")");
+
+    svg.append("text")
+        .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .attr("text-anchor", "middle")
+        .text(function(d) {
+            return d;
+        });
+
+    svg.append("text")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 15)
+        .attr('x', 10)
+        .attr('y', -5)
+        .attr("text-anchor", "under")
+        .text("Vehicles moving in Campsite 0");
+
+
+    var rect = svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#ccc")
+        .selectAll("rect")
+        .data(function(d) {
+            return d3.timeDays(new Date(d, 0, 1), new Date(d + 1, 0, 1));
+        })
+        .enter().append("rect")
+        .attr("width", cellSize)
+        .attr("height", cellSize)
+        .attr("x", function(d) {
+            return d3.timeWeek.count(d3.timeYear(d), d) * cellSize;
+        })
+        .attr("y", function(d) {
+            return d.getDay() * cellSize;
+        })
+        .datum(d3.timeFormat("%d/%m/%Y"));
+
+    svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#000")
+        .selectAll("path")
+        .data(function(d) {
+            return d3.timeMonths(new Date(d, 0, 1), new Date(d + 1, 0, 1));
+        })
+        .enter().append("path")
+        .attr("d", pathMonth);
+
+
+    d3.csv("data/sensor_data_camping0.csv", function(error, csv) {
+        if (error) throw error;
+
+        var data = d3.nest()
+            .key(function(d) {
+                return d.Day;
+            })
+            .rollup(function(d) {
+                return (d[0].Total)
+            }) // type P busyness in park
+            .object(csv);
+   
+        rect.filter(function(d) {
+                return d in data;
+            })
+            .attr("fill", function(d) {
+                return color(data[d]);
+            })
+            .append("title")
+            .text(function(d) {
+                return d + ": " + data[d];
+            });
+    });
+
+
+
+    function pathMonth(t0) {
+        var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
+            d0 = t0.getDay(),
+            w0 = d3.timeWeek.count(d3.timeYear(t0), t0),
+            d1 = t1.getDay(),
+            w1 = d3.timeWeek.count(d3.timeYear(t1), t1);
+        return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize +
+            "H" + w0 * cellSize + "V" + 7 * cellSize +
+            "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize +
+            "H" + (w1 + 1) * cellSize + "V" + 0 +
+            "H" + (w0 + 1) * cellSize + "Z";
+    }
+};
+function drawCamp1() {
+    var width = 960,
+        height = 136,
+        cellSize = 10;
+
+    var color = d3.scaleLinear()
+        .domain([0, 5]) // MUST BE RELATIVE TO MIN AND MAX
+        .range(["#fee0d2", "#de2d26"]);
+
+    var svg = d3.select("#camp1")
+        .selectAll("svg")
+        .data(d3.range(2015, 2017))
+        .enter().append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + ((width - cellSize * 53) / 2) + "," + (height - cellSize * 7 - 1) + ")");
+
+    svg.append("text")
+        .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 10)
+        .attr("text-anchor", "middle")
+        .text(function(d) {
+            return d;
+        });
+
+    svg.append("text")
+        .attr("font-family", "sans-serif")
+        .attr("font-size", 15)
+        .attr('x', 10)
+        .attr('y', -5)
+        .attr("text-anchor", "under")
+        .text("Vehicles moving in Campsite 1");
+
+
+    var rect = svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#ccc")
+        .selectAll("rect")
+        .data(function(d) {
+            return d3.timeDays(new Date(d, 0, 1), new Date(d + 1, 0, 1));
+        })
+        .enter().append("rect")
+        .attr("width", cellSize)
+        .attr("height", cellSize)
+        .attr("x", function(d) {
+            return d3.timeWeek.count(d3.timeYear(d), d) * cellSize;
+        })
+        .attr("y", function(d) {
+            return d.getDay() * cellSize;
+        })
+        .datum(d3.timeFormat("%d/%m/%Y"));
+
+    svg.append("g")
+        .attr("fill", "none")
+        .attr("stroke", "#000")
+        .selectAll("path")
+        .data(function(d) {
+            return d3.timeMonths(new Date(d, 0, 1), new Date(d + 1, 0, 1));
+        })
+        .enter().append("path")
+        .attr("d", pathMonth);
+
+
+    d3.csv("data/sensor_data_camping1.csv", function(error, csv) {
+        if (error) throw error;
+
+        var data = d3.nest()
+            .key(function(d) {
+                return d.Day;
+            })
+            .rollup(function(d) {
+                return (d[0].Total)
+            }) // type P busyness in park
+            .object(csv);
+   
+        rect.filter(function(d) {
+                return d in data;
+            })
+            .attr("fill", function(d) {
+                return color(data[d]);
+            })
+            .append("title")
+            .text(function(d) {
+                return d + ": " + data[d];
+            });
+    });
+
+
+
+    function pathMonth(t0) {
+        var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
+            d0 = t0.getDay(),
+            w0 = d3.timeWeek.count(d3.timeYear(t0), t0),
+            d1 = t1.getDay(),
+            w1 = d3.timeWeek.count(d3.timeYear(t1), t1);
+        return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize +
+            "H" + w0 * cellSize + "V" + 7 * cellSize +
+            "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize +
+            "H" + (w1 + 1) * cellSize + "V" + 0 +
+            "H" + (w0 + 1) * cellSize + "Z";
+    }
+};
